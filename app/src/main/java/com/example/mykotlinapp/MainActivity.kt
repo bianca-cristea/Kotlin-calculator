885419ac42c6588b6ad4fac836017c7403d27a61
Cristea Bianca <bianca.cristea01@yahoo.com>
2024-10-03 21:00:27 +0300

    Add some improvements in code

--- app/src/main/java/com/example/mykotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinapp/MainActivity.kt
@@ -9,38 +9,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mykotlinapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() , View.OnClickListener {
 
-    lateinit var btnAdd: Button
-    lateinit var btnSub: Button
-    lateinit var btnMultiply: Button
-    lateinit var btnDivision: Button
-    lateinit var etA:EditText
-    lateinit var etB:EditText
-    lateinit var resultTextView: TextView
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnAdd=findViewById(R.id.addition)
-        btnSub=findViewById(R.id.subtraction)
-        btnMultiply=findViewById(R.id.multiplication)
-        btnDivision=findViewById(R.id.division)
-        etA=findViewById(R.id.et_a)
-        etB=findViewById(R.id.et_b)
-        resultTextView=findViewById(R.id.result)
-
-        btnAdd.setOnClickListener(this);
-        btnSub.setOnClickListener(this);
-        btnMultiply.setOnClickListener(this);
-        btnDivision.setOnClickListener(this);
+        binding.addition.setOnClickListener(this);
+        binding.subtraction.setOnClickListener(this);
+        binding.division.setOnClickListener(this);
+        binding.multiplication.setOnClickListener(this);
     }
 
     override fun onClick(v: View?) {
-        var a = etA.text.toString().toDouble();
-        var b = etB.text.toString().toDouble();
+        var a = binding.etA.text.toString().toDouble();
+        var b = binding.etB.text.toString().toDouble();
         var result = 0.0
 
         when(v?.id){
@@ -57,6 +45,6 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
                 result=a/b
             }
         }
-        resultTextView.text="Result is $result"
+        binding.result.text="Result is $result"
     }
 }
